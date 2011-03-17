a0a745eb51c49c3cbaa0b9c0627f1ade7da0c180
Marcel Ruegenberg <dustlab@dustlab.com>
2011-03-17 15:06:35 +0100

    Added time interval to string converter.

---
+++ CoreFoundationAdditions/TimeHelpers.m
@@ -0,0 +1,20 @@
+//
+//  TimeHelpers.m
+//  Checklists
+//
+//  Created by Marcel Ruegenberg on 06.02.11.
+//  Copyright 2011 Dustlab. All rights reserved.
+//
+
+// time interval in seconds
+NSString *timeIntervalToString(NSInteger timeInterval) {
+	if(timeInterval == 0) return @"None";
+
+	NSString *prefix = @"";
+	NSString *suffix = timeInterval < 0 ? @"before" : @"after";
+	if(timeInterval < 0) timeInterval = timeInterval * (-1);
+	if(timeInterval > 60 * 60 * 24) prefix = [NSString stringWithFormat:@"%d days", (timeInterval / (60 * 60 * 24))];
+	else if(timeInterval > 60 * 60) prefix = [NSString stringWithFormat:@"%d hours", (timeInterval / (60 * 60))];
+	else prefix = [NSString stringWithFormat:@"%d minutes", (timeInterval / 60)];
+	return [NSString stringWithFormat:@"%@ %@", prefix, suffix];
+}
