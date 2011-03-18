cd4119b7acfa023095710e687a80e02b48052b8b
Yevhene Shemet <eshemet@componentix.com>
2011-03-19 01:02:07 +0200

    Added PercentEscape NSString addition.

---
+++ CoreFoundationAdditions/NSString+PercentEscape.h
@@ -0,0 +1,13 @@
+//
+//  NSString+PercentEscape.h
+//
+//  Created by Yevhene Shemet on 10.08.10.
+//  Copyright Componentix 2010. All rights reserved.
+//
+
+
+@interface NSString (PercentEscape)
+
+- (NSString *)stringWithPercentEscape;
+
+@end
---
+++ CoreFoundationAdditions/NSString+PercentEscape.m
@@ -0,0 +1,23 @@
+//
+//  NSString+Escape.m
+//
+//  Created by Yevhene Shemet on 10.08.10.
+//  Copyright Componentix 2010. All rights reserved.
+//
+
+
+#import "NSString+Escape.h"
+
+
+@implementation NSString (PercentEscape)
+
+- (NSString *)stringWithPercentEscape {
+    return [(NSString *)CFURLCreateStringByAddingPercentEscapes(
+        NULL,
+        (CFStringRef)[[self mutableCopy] autorelease],
+        NULL,
+        CFSTR("￼=,!$&'()*+;@?\n\"<>#\t :/"),
+        kCFStringEncodingUTF8) autorelease];
+}
+
+@end
