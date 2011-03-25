121461b59e4550939bde13212590a311e43c889a
Marcel Ruegenberg <dustlab@dustlab.com>
2011-03-25 23:51:14 +0100

    Simplified UIImage+RoundedCorner to use existing rounded rectangle code.

--- UIKitAdditions/UIImage+RoundedCorner.m
+++ UIKitAdditions/UIImage+RoundedCorner.m
@@ -4,12 +4,9 @@
 // No warranty is expressed or implied.
 
 #import "UIImage+RoundedCorner.h"
-#import "UIImage+Alpha.h"
+#import "UIImage+Additions.h"
+#import "UIKit+DrawingHelpers.h"
 
-// Private helper methods
-@interface UIImage ()
-- (void)addRoundedRectToPath:(CGRect)rect context:(CGContextRef)context ovalWidth:(CGFloat)ovalWidth ovalHeight:(CGFloat)ovalHeight;
-@end
 
 @implementation UIImage (RoundedCorner)
 
@@ -30,13 +27,7 @@
                                                  CGImageGetBitmapInfo(image.CGImage));
 
     // Create a clipping path with rounded corners
-    CGContextBeginPath(context);
-    [self addRoundedRectToPath:CGRectMake(borderSize, borderSize, image.size.width - borderSize * 2, image.size.height - borderSize * 2)
-                       context:context
-                     ovalWidth:cornerSize
-                    ovalHeight:cornerSize];
-    CGContextClosePath(context);
-    CGContextClip(context);
+    CGContextClipToRoundedRect(context, CGRectMake(borderSize, borderSize, image.size.width, image.size.height), cornerSize);
 
     // Draw the image to the context; the clipping path will make anything outside the rounded rect transparent
     CGContextDrawImage(context, CGRectMake(0, 0, image.size.width, image.size.height), image.CGImage);
@@ -50,30 +41,6 @@
     CGImageRelease(clippedImage);
     
     return roundedImage;
-}
-
-#pragma mark -
-#pragma mark Private helper methods
-
-// Adds a rectangular path to the given context and rounds its corners by the given extents
-// Original author: Björn Sållarp. Used with permission. See: http://blog.sallarp.com/iphone-uiimage-round-corners/
-- (void)addRoundedRectToPath:(CGRect)rect context:(CGContextRef)context ovalWidth:(CGFloat)ovalWidth ovalHeight:(CGFloat)ovalHeight {
-    if (ovalWidth == 0 || ovalHeight == 0) {
-        CGContextAddRect(context, rect);
-        return;
-    }
-    CGContextSaveGState(context);
-    CGContextTranslateCTM(context, CGRectGetMinX(rect), CGRectGetMinY(rect));
-    CGContextScaleCTM(context, ovalWidth, ovalHeight);
-    CGFloat fw = CGRectGetWidth(rect) / ovalWidth;
-    CGFloat fh = CGRectGetHeight(rect) / ovalHeight;
-    CGContextMoveToPoint(context, fw, fh/2);
-    CGContextAddArcToPoint(context, fw, fh, fw/2, fh, 1);
-    CGContextAddArcToPoint(context, 0, fh, 0, fh/2, 1);
-    CGContextAddArcToPoint(context, 0, 0, fw/2, 0, 1);
-    CGContextAddArcToPoint(context, fw, 0, fw, fh/2, 1);
-    CGContextClosePath(context);
-    CGContextRestoreGState(context);
 }
 
 @end
