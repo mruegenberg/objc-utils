5170fcc65dceb9da88da73e3e97cb2d7b9998554
Marcel Ruegenberg <dustlab@dustlab.com>
2011-06-11 20:13:38 +0200

    Added category that allows hiding of UITabBar.

---
+++ UIKitAdditions/UITabBarController+HideableTabBar.m
@@ -0,0 +1,45 @@
+//
+//  UITabBar+HidableTabBar.m
+//  TBHidingTest
+//
+//  Created by Marcel Ruegenberg on 11.06.11.
+//  Copyright 2011 Dustlab. All rights reserved.
+//
+
+#import "UITabBarController+HideableTabBar.h"
+
+
+@implementation UITabBarController (HideableTabBar)
+
+- (void)setTabBarHidden:(BOOL)hidden {
+    UIView *contentView = nil;
+    
+    NSArray *subviews = self.view.subviews;
+    NSAssert([subviews count] >= 2, @"Structural problem in implementation of Tab bar hiding.");
+    
+    // contentView is the subview that is not the tab bar
+    if([[subviews objectAtIndex:0] isKindOfClass:[UITabBar class]])
+        contentView = [subviews objectAtIndex:1];
+    else
+        contentView = [subviews objectAtIndex:0];
+    
+    [UIView animateWithDuration:0.3 animations:^{
+        CGRect f = self.tabBar.frame;
+        
+        if (hidden) {
+            contentView.frame = self.view.bounds;
+            f.origin.y = self.view.bounds.origin.y + self.view.bounds.size.height;
+        }
+        else {
+            contentView.frame = CGRectMake(self.view.bounds.origin.x,
+                                           self.view.bounds.origin.y,
+                                           self.view.bounds.size.width,
+                                           self.view.bounds.size.height - self.tabBar.frame.size.height);
+            f.origin.y = self.view.bounds.origin.y + self.view.bounds.size.height - f.size.height;
+        }
+        
+        self.tabBar.frame = f;
+    }];
+}
+
+@end
