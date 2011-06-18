b4aae2b3691629b5218a8e3349a9e528efabf649
Marcel Ruegenberg <dustlab@dustlab.com>
2011-06-19 01:13:15 +0200

    Added simple bindings with transformer blocks to easily react to value changes without the whole baggage of KVO; Fixed a bug in existing simple bindings that could lead to deallocated objects being observed.

--- CoreFoundationAdditions/NSObject+SimpleBindings.h
+++ CoreFoundationAdditions/NSObject+SimpleBindings.h
@@ -8,6 +8,8 @@
 
 #import <Foundation/Foundation.h>
 
+typedef id(^SimpleBindingTransformer)(id value);
+
 /**
  A simple bindings-like mechanism. Inspired by Cocoa bindings.
  No value transformers are supported. 
@@ -18,13 +20,25 @@
 
 /**
  Ensure that the own value for a key is always equal to the value for a keyPath of another object.
+ Works both ways, i.e the keypath of the other object is also updated if it changes in self.
  */
 - (void)bind:(NSString *)binding toKeyPath:(NSString *)keyPath ofObject:(id)object;
 
 /**
  Removes the binding from all key paths of object to this object.
+ You must call this method on the same object that you called -[bind:toKeyPath:ofObject:] on.
+ Even though the bindings work both ways for most purposes, the do not for memory management.
  */
 - (void)unbindObject:(NSObject *)object;
+
+/**
+ Ensure that the own value for a key is always equal to the transformed value for a keyPath of another object.
+ Works only one way, i.e the key path is not set for the other object if the bound value in this object changes.
+ @param transformer A block that takes the value for keyPath of object and returns the value that the keypath represented by binding of self should be set to.
+ */
+- (void)bind:(NSString *)binding toKeyPath:(NSString *)keyPath ofObject:(id)object withTransformer:(SimpleBindingTransformer)transfomer;
+
+- (void)unbindObjectTransformed:(NSObject *)object;
 
 
 @end
--- CoreFoundationAdditions/NSObject+SimpleBindings.m
+++ CoreFoundationAdditions/NSObject+SimpleBindings.m
@@ -26,10 +26,12 @@
 
 - (NSObject *)otherObject:(NSObject *)obj;
 
+@property BOOL bindingActive;
+
 @end
 
 @implementation ObjectBinding
-@synthesize obj1, keyPath1, obj2, keyPath2;
+@synthesize obj1, keyPath1, obj2, keyPath2, bindingActive;
 
 - (id)initWithKeyPath:(NSString *)keyPath1_ ofObj:(NSObject *)obj1_ keyPath:(NSString *)keyPath2_ ofObj:(NSObject *)obj2_ {
     if((self = [super init])) {
@@ -37,6 +39,7 @@
         self.obj2 = obj2_; self.keyPath2 = keyPath2_;
         [self.obj1 addObserver:self forKeyPath:self.keyPath1 options:0 context:NULL];
         [self.obj2 addObserver:self forKeyPath:self.keyPath2 options:0 context:NULL];
+		self.bindingActive = YES;
     }
     return self;
 }
@@ -44,6 +47,7 @@
 - (void)deactivateBinding {
     [self.obj1 removeObserver:self forKeyPath:self.keyPath1];
     [self.obj2 removeObserver:self forKeyPath:self.keyPath2];
+	self.bindingActive = NO;
 }
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
@@ -70,6 +74,7 @@
 }
 
 - (void)dealloc {
+	if(self.bindingActive) [self deactivateBinding];
     self.obj1 = nil;
     self.keyPath1 = nil;
     self.obj2 = nil;
@@ -79,12 +84,49 @@
 
 @end
 
+@interface TransformedObjectBinding : ObjectBinding {
+}
 
-@interface NSObject (SimpleBindingsPrivate)
+@property (copy) SimpleBindingTransformer transformer;
 
-- (void)bindOtherObject:(ObjectBinding *)binding;
+- (id)initWithKeyPath:(NSString *)keyPath1 ofObj:(NSObject *)obj1 keyPath:(NSString *)keyPath2 ofObj:(NSObject *)obj2 withTransformer:(SimpleBindingTransformer)transformer;
 
-- (void)unbindOtherObject:(ObjectBinding *)binding;
+@end
+
+@implementation TransformedObjectBinding
+@synthesize transformer;
+
+- (id)initWithKeyPath:(NSString *)keyPath1_ ofObj:(NSObject *)obj1_ keyPath:(NSString *)keyPath2_ ofObj:(NSObject *)obj2_ withTransformer:(SimpleBindingTransformer)transformer_ {
+    if((self = [super init])) {
+        self.obj1 = obj1_; self.keyPath1 = keyPath1_;
+        self.obj2 = obj2_; self.keyPath2 = keyPath2_;
+        self.transformer = transformer_;
+		[self.obj1 setValue:(self.transformer([self.obj2 valueForKeyPath:self.keyPath2])) forKeyPath:self.keyPath1];
+        [self.obj2 addObserver:self forKeyPath:self.keyPath2 options:0 context:NULL];
+    }
+    return self;
+}
+
+- (void)deactivateBinding {
+    [self.obj2 removeObserver:self forKeyPath:self.keyPath2];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    NSAssert(object == self.obj2 && [keyPath isEqualToString:self.keyPath2], @"Error in simple bindings mechanism.");
+    
+    id val = [object valueForKeyPath:keyPath];
+    if(self.transformer)
+        val = self.transformer(val);
+    
+    if(! [[self.obj1 valueForKeyPath:self.keyPath1] isEqual:val]) {
+        [self.obj1 setValue:val forKeyPath:self.keyPath1];
+    }
+}
+
+- (void)dealloc {
+    self.transformer = nil;
+    [super dealloc];
+}
 
 @end
 
@@ -104,8 +146,7 @@ static char bindingsKey;
     
     ObjectBinding *b = [[ObjectBinding alloc] initWithKeyPath:binding ofObj:self keyPath:keyPath ofObj:object];
     [bindings addObject:b];
-    
-    [object bindOtherObject:b];
+	
     [b release];
 }
 
@@ -117,36 +158,41 @@ static char bindingsKey;
     for(ObjectBinding *b in bindings) {
         if([b containsObject:object]) {
             [b deactivateBinding];
-            [[b otherObject:self] unbindOtherObject:b];
             [bindingsToRemove addObject:b];
         }
     }
     [bindings minusSet:bindingsToRemove];
-    
 }
 
-@end
+static char transformedBindingsKey;
 
-
-@implementation NSObject (SimpleBindingsPrivate)
-
-- (void)bindOtherObject:(ObjectBinding *)binding {
-    NSMutableSet *bindings = objc_getAssociatedObject(self, &bindingsKey);
+- (void)bind:(NSString *)binding toKeyPath:(NSString *)keyPath ofObject:(id)object withTransformer:(SimpleBindingTransformer)transformer {
+    // see "Associative references" in the docs for details
+    NSMutableSet *bindings = objc_getAssociatedObject(self, &transformedBindingsKey);
     if(bindings == nil) {
         bindings = [NSMutableSet new];
-        objc_setAssociatedObject(self, &bindingsKey, bindings, OBJC_ASSOCIATION_RETAIN);
+        objc_setAssociatedObject(self, &transformedBindingsKey, bindings, OBJC_ASSOCIATION_RETAIN);
         [bindings release];
     }
     
-    [bindings addObject:binding];
+    TransformedObjectBinding *b = [[TransformedObjectBinding alloc] initWithKeyPath:binding ofObj:self keyPath:keyPath ofObj:object withTransformer:transformer];
+    [bindings addObject:b];
+    
+    [b release];
 }
 
-- (void)unbindOtherObject:(ObjectBinding *)binding {
-    NSMutableSet *bindings = objc_getAssociatedObject(self, &bindingsKey);
+- (void)unbindObjectTransformed:(NSObject *)object {
+    NSMutableSet *bindings = objc_getAssociatedObject(self, &transformedBindingsKey);
     if(! bindings) return;
     
-    [bindings removeObject:binding];
+    NSMutableSet *bindingsToRemove = [NSMutableSet set];
+    for(TransformedObjectBinding *b in bindings) {
+        if([b containsObject:object]) {
+            [b deactivateBinding];
+            [bindingsToRemove addObject:b];
+        }
+    }
+    [bindings minusSet:bindingsToRemove];
 }
 
 @end
-
