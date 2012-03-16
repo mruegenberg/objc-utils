8719b18ba34567bc21fc3e7a891d0c2bf3c8f09c
Marcel Ruegenberg <dustlab@dustlab.com>
2012-03-16 21:26:10 +0100

    Possibility to make tab bar "flat" (e.g for landscape).

--- UIKitAdditions/UITabBarController+HideableTabBar.h
+++ UIKitAdditions/UITabBarController+HideableTabBar.h
@@ -7,11 +7,11 @@
 //  
 //
 
-#import <UIKit/UIKit.h>
-
 //  WARNING: The implementation of this category makes certain assumptions about the structure of the subviews of a UITabBarController's view. There is no guarantee for this to keep working from one version of iOS to the next.
 @interface UITabBarController (HideableTabBar)
 
 - (void)setTabBarHidden:(BOOL)hidden;
+
+- (void)setTabBarFlat:(BOOL)flat;
 
 @end
--- UIKitAdditions/UITabBarController+HideableTabBar.m
+++ UIKitAdditions/UITabBarController+HideableTabBar.m
@@ -34,9 +34,36 @@
             contentView.frame = CGRectMake(self.view.bounds.origin.x,
                                            self.view.bounds.origin.y,
                                            self.view.bounds.size.width,
-                                           self.view.bounds.size.height - self.tabBar.frame.size.height);
+                                           self.view.bounds.size.height - f.size.height);
             f.origin.y = self.view.bounds.origin.y + self.view.bounds.size.height - f.size.height;
         }
+        
+        self.tabBar.frame = f;
+    }];
+}
+
+- (void)setTabBarFlat:(BOOL)flat {
+    CGFloat h = flat ? 39 : 49;
+    UIView *contentView = nil;
+    
+    NSArray *subviews = self.view.subviews;
+    NSAssert([subviews count] >= 2, @"Structural problem in implementation of Tab bar hiding.");
+    
+    // contentView is the subview that is not the tab bar
+    if([[subviews objectAtIndex:0] isKindOfClass:[UITabBar class]])
+        contentView = [subviews objectAtIndex:1];
+    else
+        contentView = [subviews objectAtIndex:0];
+    
+    [UIView animateWithDuration:0.3 animations:^{
+        CGRect f = self.tabBar.frame;
+        f.size.height = h;
+        
+        contentView.frame = CGRectMake(self.view.bounds.origin.x,
+                                       self.view.bounds.origin.y,
+                                       self.view.bounds.size.width,
+                                       self.view.bounds.size.height - f.size.height);
+        f.origin.y = self.view.bounds.origin.y + self.view.bounds.size.height - f.size.height;
         
         self.tabBar.frame = f;
     }];
