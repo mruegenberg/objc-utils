0ed37543e6b121b05ff609baa83579e22a447ae2
Marcel Ruegenberg <subw@dustlab.com>
2012-10-08 00:48:40 +0200

    Improved block-based KVO utile.

--- CoreFoundation/NSObject/NSObject+ObserveActions.h
+++ CoreFoundation/NSObject/NSObject+ObserveActions.h
@@ -17,9 +17,15 @@ typedef void(^ObserveAction)(void);
 
 @interface NSObject (ObserveActions)
 
-- (void)addObserverAction:(ObserveAction)action forKeyPath:(NSString *)keyPath;
+// add an observer action for a keypath. if an observer with the same context already exists, that observer is overwritten.
+// usually pass the object from which you call the method as a reference both when adding and removing the observer.
+- (void)addObserverAction:(ObserveAction)action forKeyPath:(NSString *)keyPath context:(id)context;
 
-// TODO: make this dependent on the observing object!
-- (void)removeActionObserverForKeyPath:(NSString *)keyPath;
+//- (void)addObserverAction:(ObserveAction)action forKeyPath:(NSString *)keyPath; // for backwards compatibility. uses `nil` as the context.
+
+// remove the action observer for the corresponding keypath with a specific context (which may be nil)
+- (void)removeActionObserverForKeyPath:(NSString *)keyPath context:(id)context;
+
+- (void)removeActionObserverForKeyPath:(NSString *)keyPath; // for backwards compatibility. uses `nil` as the context.
 
 @end
--- CoreFoundation/NSObject/NSObject+ObserveActions.m
+++ CoreFoundation/NSObject/NSObject+ObserveActions.m
@@ -17,33 +17,35 @@
 @property (copy) NSString *keyPath;
 @property (copy) ObserveAction action;
 @property BOOL bindingActive;
+@property (retain) id context;
 
-- (id)initWithObservedObject:(NSObject *)observedObj keyPath:(NSString *)keyPath action:(ObserveAction)action;
+- (id)initWithObservedObject:(NSObject *)observedObj keyPath:(NSString *)keyPath action:(ObserveAction)action context:(id)context;
 - (void)deactivateBinding;
 
 @end
 
 @implementation ObserveActionBinding
-@synthesize observedObj, keyPath, action, bindingActive;
+@synthesize observedObj, keyPath, action, bindingActive, context;
 
-- (id)initWithObservedObject:(NSObject *)_observedObj keyPath:(NSString *)_keyPath action:(ObserveAction)_action {
+- (id)initWithObservedObject:(NSObject *)_observedObj keyPath:(NSString *)_keyPath action:(ObserveAction)_action context:(id)_context {
 	if((self = [super init])) {
-		self.observedObj = _observedObj; self.keyPath = _keyPath; self.action = _action;
-		[self.observedObj addObserver:self forKeyPath:self.keyPath options:0 context:NULL];
+		self.observedObj = _observedObj; self.keyPath = _keyPath; self.action = _action; self.context = _context;
+		[self.observedObj addObserver:self forKeyPath:self.keyPath options:0 context:(__bridge void *)(_context)]; // passing the context is not really necessary
 		bindingActive = YES;
 	}
 	return self;
 }
 
-- (void)observeValueForKeyPath:(NSString *)_keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+- (void)observeValueForKeyPath:(NSString *)_keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)_context {
 	NSAssert2([_keyPath isEqualToString:self.keyPath], @"Action binding called with invalid keyPath %@ instead of ", _keyPath, self.keyPath);
 	NSAssert(object == self.observedObj, @"Action binding called with invalid object");
+    NSAssert(_context == (__bridge void *)(self.context), @"Action binding called with wrong context");
 	self.action();
 }	
 
 - (void)deactivateBinding {
 	if(bindingActive) {
-		[self.observedObj removeObserver:self forKeyPath:self.keyPath];
+        [self.observedObj removeObserver:self forKeyPath:self.keyPath context:(__bridge void *)(self.context)];
 		bindingActive = NO;
 	}
 }
@@ -62,30 +64,53 @@
 
 static char observeActionKey;
 
-- (void)addObserverAction:(ObserveAction)action forKeyPath:(NSString *)keyPath {
+- (void)addObserverAction:(ObserveAction)action forKeyPath:(NSString *)keyPath context:(id)context {
 	// see "Associative references" in the docs for details
-    NSMutableDictionary *actions = objc_getAssociatedObject(self, &observeActionKey);
+    NSMutableArray *actions = objc_getAssociatedObject(self, &observeActionKey);
     if(actions == nil) {
-        actions = [NSMutableDictionary new];
+        actions = [NSMutableArray new];
         objc_setAssociatedObject(self, &observeActionKey, actions, OBJC_ASSOCIATION_RETAIN);
     }
 	
-	ObserveActionBinding *binding = [[ObserveActionBinding alloc] initWithObservedObject:self keyPath:keyPath action:action];
-	ObserveActionBinding *prevBinding = [actions objectForKey:keyPath];
-	[prevBinding deactivateBinding]; // technically not necessary
-	[actions setObject:binding forKey:keyPath];
+	ObserveActionBinding *binding = [[ObserveActionBinding alloc] initWithObservedObject:self keyPath:keyPath action:action context:context];
+    
+    NSUInteger c = [actions count];
+    for(int i = 0; i < c; ++i) {
+        ObserveActionBinding *b = [actions objectAtIndex:i];
+        if([b.keyPath isEqualToString:keyPath] && b.context == context) {
+            [b deactivateBinding];
+            [actions removeObjectAtIndex:i];
+            break;
+        }
+    }
+    
+    [actions addObject:binding];
+}
+
+- (void)addObserverAction:(ObserveAction)action forKeyPath:(NSString *)keyPath {
+    [self addObserverAction:action forKeyPath:keyPath context:nil];
+}
+
+- (void)removeActionObserverForKeyPath:(NSString *)keyPath context:(id)context {
+	NSMutableArray *actions = objc_getAssociatedObject(self, &observeActionKey);
+    if(actions == nil) {
+        actions = [NSMutableArray new];
+        objc_setAssociatedObject(self, &observeActionKey, actions, OBJC_ASSOCIATION_RETAIN);
+    }
+	
+    NSUInteger c = [actions count];
+    for(int i = 0; i < c; ++i) {
+        ObserveActionBinding *b = [actions objectAtIndex:i];
+        if([b.keyPath isEqualToString:keyPath] && b.context == context) {
+            [b deactivateBinding];
+            [actions removeObjectAtIndex:i];
+            break;
+        }
+    }
 }
 
 - (void)removeActionObserverForKeyPath:(NSString *)keyPath {
-	NSMutableDictionary *actions = objc_getAssociatedObject(self, &observeActionKey);
-    if(actions == nil) {
-        actions = [NSMutableDictionary new];
-        objc_setAssociatedObject(self, &observeActionKey, actions, OBJC_ASSOCIATION_RETAIN);
-    }
-	
-	ObserveActionBinding *binding = [actions objectForKey:keyPath];
-	[binding deactivateBinding]; // technically not necessary
-	[actions removeObjectForKey:keyPath];
+    [self removeActionObserverForKeyPath:keyPath context:nil];
 }
 
 @end
