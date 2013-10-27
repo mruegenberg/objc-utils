cc475e95393698a55d162a869f57581c952de2b7
Marcel Ruegenberg <dustlab@dustlab.com>
2013-10-27 15:05:20 +0100

    Removed deprecation to fix Cocoapods.

--- CoreFoundation/NSObject/NSObject+ObserveActions.h
+++ CoreFoundation/NSObject/NSObject+ObserveActions.h
@@ -22,12 +22,14 @@ typedef void(^ObserveAction)(void);
 - (void)addObserverAction:(ObserveAction)action forKeyPath:(NSString *)keyPath context:(id)context;
 
 // for backwards compatibility. uses `nil` as the context.
-- (void)addObserverAction:(ObserveAction)action forKeyPath:(NSString *)keyPath DEPRECATED_MSG_ATTRIBUTE("Supersed by -[addObserverAction:forKeyPath:context:]. Will be removed in a future release.");
+- (void)addObserverAction:(ObserveAction)action forKeyPath:(NSString *)keyPath;
+// DEPRECATED_MSG_ATTRIBUTE("Supersed by -[addObserverAction:forKeyPath:context:]. Will be removed in a future release.");
 
 // remove the action observer for the corresponding keypath with a specific context (which may be nil)
 - (void)removeActionObserverForKeyPath:(NSString *)keyPath context:(id)context;
 
 // for backwards compatibility. uses `nil` as the context.
-- (void)removeActionObserverForKeyPath:(NSString *)keyPath DEPRECATED_MSG_ATTRIBUTE("Supersed by -[removeActionObserverForKeyPath:context:]. Will be removed in a future release."); 
+- (void)removeActionObserverForKeyPath:(NSString *)keyPath;
+// DEPRECATED_MSG_ATTRIBUTE("Supersed by -[removeActionObserverForKeyPath:context:]. Will be removed in a future release.");
 
 @end
